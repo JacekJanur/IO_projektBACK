956a69e315f66be47eebff8ead7fd0c045a2a70d
JacekJanur <jacekjanur1@gmail.com>
2022-05-05 10:41:24 +0200

    added API for showing single user

--- users/models.py
+++ users/models.py
@@ -5,5 +5,3 @@ class User(models.Model):
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
     token = models.CharField(max_length=200, default=None, blank=True, null=True)
-    def __str__(self):
-        return self.email
--- users/views.py
+++ users/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import User
 
 def index(request):
     return HttpResponse(serializers.serialize('json', User.objects.all()))
+
+def detail(request, user_id):
+
+    if User.objects.filter(pk=user_id).exists():
+        user = User.objects.get(pk=user_id)
+        ret = HttpResponse(serializers.serialize("json", [user]), content_type='application/json')
+    else:
+        ret =  JsonResponse({'status':404,'message':"user not found"})
+    return ret
+

9a278d71439f219a33891e8806df06d181e5ffcf
JacekJanur <jacekjanur1@gmail.com>
2022-05-06 10:39:42 +0200

    review model

--- games/models.py
+++ games/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from genres.models import Genre
+from users.models import User
 
 class Game(models.Model):
     name = models.CharField(max_length=200)
--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,9 @@
-# from django.db import models
-# from users.models import User
-# from games.models import Game
+from django.db import models
 
-# class Review(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-#     review = models.IntegerField(max_length=1, blank=True, null=True)
+class Review(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    game = models.ForeignKey('games.Game', on_delete=models.CASCADE)
+    review = models.IntegerField(blank=True, null=True)
 
-#     class Meta:
-#         unique_together = [['user', 'game']]
+    class Meta:
+        unique_together = [['user', 'game']]
